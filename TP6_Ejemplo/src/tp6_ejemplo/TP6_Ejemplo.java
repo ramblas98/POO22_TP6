c58e64e4c2ee6d341a427cf41d936353db0d9ba0
profeblas <profeblas@localhost>
2022-06-15 18:45:48 -0300

    TP6 - Ejemplos

---
+++ TP6_Ejemplo/src/tp6_ejemplo/TP6_Ejemplo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp6_ejemplo;
+
+/**
+ *
+ * @author profeblas
+ */
+public class TP6_Ejemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
